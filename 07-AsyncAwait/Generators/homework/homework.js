897fb2b3421514e7860e805cd21437a980159704
Parricio <patriciogabriel colella@gmail.com>
2022-05-16 13:03:19 -0300

    homework 07 finished

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,19 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
-
+  
+  num=1
+  while(num<=max||!max){
+    if(num%3===0&&num%5===0){
+      yield "Fizz Buzz"
+    }else{
+      if(num%3===0) yield "Fizz"
+      else{
+        if(num%5===0) yield "Buzz"
+        else yield num
+      }
+    }
+    num=num+1
+  }
 }
 
 module.exports = fizzBuzzGenerator;
