8fa6cdbacbfadcd742bcb23021be83f4deabc962
Parricio <patriciogabriel colella@gmail.com>
2022-05-16 13:03:12 -0300

    homework 06 finished

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -9,10 +9,47 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/test', (req, res) => {
+  res.send({
+    message: 'hola',
+  });
+});
+
+app.post('/sum', (req, res) => {
+  res.send({
+    result: req.body.a+req.body.b
+  });
+});
+
 app.post('/product', (req, res) => {
   res.send({
-    result: req.body.a / req.body.b,
+    result: req.body.a * req.body.b,
   });
+});
+
+app.post('/sumArray', (req, res) => {
+  var arr=req.body.array
+  var n=req.body.num
+  var si=false
+  for(var i=0;i<arr.length;i++){
+    for(var x=i+1;x<arr.length;x++){
+      if(arr[i]+arr[x]===n)si=true
+    }
+  }
+  res.send({result:si})
+});
+
+app.post('/numstring', (req, res) => {
+  var l=req.body.string
+  if(typeof l==="number"||l.length===0)res.status(400).send()
+  else res.status(200).send({ result:l.length, });
+});
+
+app.post('/pluck', (req, res) => {
+  var {obj,propiedad}=req.body
+  
+  if(typeof obj!=="object"||propiedad.length===0)res.status(400).send()
+  else res.status(200).send({ array:[obj[propiedad]] });
 });
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -49,8 +49,54 @@ describe('Test de APIS', () => {
         .send({array: [2,5,7,10,11,15,20], num: 13})
         .then((res) => {
           expect(res.body.result).toEqual(true);
+          expect(res.status).toEqual(200)
       }));
-  });
 
+    it(`responds with false fi does not found`,()=>
+      agent.post('/sumArray')
+      .send({array: [2,5,7,10,11,15,20], num: 50})
+      .then((res) => {
+        expect(res.body.result).toEqual(false);
+      })
+    )
+  })
+  describe("POST /NumString",()=>{
+    it(`responds with 4 if string is "hola"`,()=>
+      agent.post("/numstring")
+        .send({string:"hola"})
+        .then((res)=>{
+          expect(res.body.result).toEqual(4)
+          expect(res.status).toEqual(200)
+        })
+    )
+    
+    it(`responds with 400 if string empty`,()=>
+      agent.post("/numstring").send({string:""}).expect(400)
+    )
+
+    it(`responds with 400 if string a number`,()=>
+      agent.post("/numstring").send({string:4}).expect(400)
+    )
+  })
+
+  describe("POST /pluck",()=>{
+    it(`responds with: status=200 and works fine`,()=>
+      agent.post("/pluck")
+        .send({obj:{nombre:"pepe",apellido:"rosas"},propiedad:"nombre"})
+        .then((res)=>{
+          expect(res.body.array).toEqual(["pepe"])
+          expect(res.status).toEqual(200)
+        })
+    )
+
+    it(`responds with 400 if typeof array isnt array`,()=>
+      agent.post("/pluck").send({obj:"hola",propiedad:"hola"}).expect(400)
+    )
+
+    it(`responds with 400 if dosnt hav string`,()=>
+      agent.post("/pluck").send({obj:{nombre:"pepe",apellido:"rosas"},propiedad:""}).expect(400)
+    )
+
+  })
 });
 
